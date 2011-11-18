329e624d3d0e054435a60d8f651025057ef2993a
Patrice Brend'amour <dr.allgood@brendamour.de>
2011-11-18 22:31:49 +0000

    - wrong URLs in zencoderAPIVersion enum
    git-svn-id: http://zencoder-java.googlecode.com/svn/trunk@39 803c8715-f8cf-ae66-6ab9-44f18288566c

--- src/main/java/de/bitzeche/video/transcoding/zencoder/enums/ZencoderAPIVersion.java
+++ src/main/java/de/bitzeche/video/transcoding/zencoder/enums/ZencoderAPIVersion.java
@@ -1,9 +1,9 @@
 package de.bitzeche.video.transcoding.zencoder.enums;
 
 public enum ZencoderAPIVersion {
-	API_DEV("https://app.zencoder.com/api/"), API_V1(
-			"https://app.zencoder.com/api/v1/"), API_V2(
-			"https://app.zencoder.com/api/v1/");
+	API_DEV("https://app.zencoder.com/api/"), 
+	API_V1(	"https://app.zencoder.com/api/v1/"), 
+	API_V2(	"https://app.zencoder.com/api/v2/");
 
 	private final String baseUrl;
 
